a0897f72d46264ccd930f3bd5f73d4d898f89f24
Eren <yasar451278@gmail.com>
2024-10-15 18:52:21 +0300

    up

---
+++ main.js
@@ -0,0 +1,13 @@
+let mousePos={x:0,y:0};
+    HEIGHT=window.innerHeight;
+    WIDTH =window.innerWidth;
+    flag =true;
+
+const Pi=Math.PI,
+    scene = new THREE.Scene(),
+    camera = new THREE.PerspectiveCamera(60, WIDTH / HEIGHT, 0.1, 1000),
+    asdsadas
+    asdsadasasd
+    asdsadas
+    MediaStreamAudioSourceNode
+    
---
+++ namespa.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace namespacetest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
