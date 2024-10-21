a17ec2fef533aa9b7478aad9ff2ee889c9205059
Eren <yasar451278@gmail.com>
2024-10-21 13:18:23 +0300

    up

--- main.js
+++ main.js
@@ -6,8 +6,5 @@ let mousePos={x:0,y:0};
 const Pi=Math.PI,
     scene = new THREE.Scene(),
     camera = new THREE.PerspectiveCamera(60, WIDTH / HEIGHT, 0.1, 1000),
-    asdsadas
-    asdsadasasd
-    asdsadas
-    MediaStreamAudioSourceNode
+    
     
